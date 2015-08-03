8fab36fe67d590c0f12e3eed14bb1a6f70b5f7d5
Ivo Georgiev <ivo@linvo.org>
2015-08-03 18:41:53 +0300

    NSView: must do several tweaks to it to get it to work

--- index.js
+++ index.js
@@ -116,10 +116,16 @@ function Texture(gl, width, height, type) {
 }
 */
 
+$.import('GLKit');
+
 var win = new Window();
 var view = $.NSView('alloc')('init');
+// var opengl = $.NSOpenGLView('alloc')('initWithFrame', $.NSMakeRect(0,0,400,400), 'pixelFormat', $.NSOpenGLView('defaultPixelFormat'));
+
 // figure out how to get the pointer to view
 win.native('contentView')('addSubview', view);
+view('setAutoresizingMask', $.NSViewHeightSizable | $.NSViewWidthSizable);
+// TODO make nsView opaque
 win.visible = true;
 
 
@@ -130,10 +136,7 @@ win.visible = true;
 
 
 var WebChimera = require("./webchimera.js");
-var player = WebChimera.createPlayer([ /* "-vvv" */ ]);
-player.onFrameSetup = function(width, height, pixelFormat) { 
-  console.log("frame setup",width,height) 
-};
+var player = WebChimera.createPlayer([ /* "-vvv"  */ ]);
 //player.onFrameReady = function(frame) { typeof(display)=="function" && display() };
 player.setVout(view.pointer);
 player.play("file:///Users/ivogeorgiev/Downloads/1.mkv");
