9e066cccf1a909f344cd2cf21c5395b5585d7f04
Ivo Georgiev <ivo@linvo.org>
2015-08-03 18:56:03 +0300

    Use contentView NSView directly for now, although to make it work with VLC we have to override two methods

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ if (process.bridge && process.bridge.objc) { // so we can also test this in node
 var $ = process.bridge.objc;
 
 
-//$.import('GLKit');
-
 var win = new Window();
-var view = $.NSView('alloc')('init');
-// var opengl = $.NSOpenGLView('alloc')('initWithFrame', $.NSMakeRect(0,0,400,400), 'pixelFormat', $.NSOpenGLView('defaultPixelFormat'));
+win.visible = true;
+win.backgroundColor = "black";
 
+/*
+// Doing it with a sub-view
+var view = $.NSView('alloc')('initWithFrame', $.NSMakeRect(0, 0, 400, 400));
+// var opengl = $.NSOpenGLView('alloc')('initWithFrame', $.NSMakeRect(0,0,400,400), 'pixelFormat', $.NSOpenGLView('defaultPixelFormat'));
 // figure out how to get the pointer to view
 win.native('contentView')('addSubview', view);
+win.native('contentView')('setAutoresizesSubviews', $.YES);
 view('setAutoresizingMask', $.NSViewHeightSizable | $.NSViewWidthSizable);
 // TODO make nsView opaque
-win.visible = true;
+*/
 
-
-
-
+var view = win.native('contentView');
+//view('setOpaque', $.YES)
+//view('setBackgroundColor', $.NSColor('blackColor'));
 
 }
 
@@ -25,7 +28,7 @@ win.visible = true;
 var WebChimera = require("./webchimera.js");
 var player = WebChimera.createPlayer([ /* "-vvv"  */ ]);
 //player.onFrameReady = function(frame) { typeof(display)=="function" && display() };
-player.setVout(view.pointer);
+//if (view) player.setVout(view.pointer);
 player.play("file:///Users/ivogeorgiev/Downloads/1.mkv");
 global.player = player;
 
