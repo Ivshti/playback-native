bf5dee47e6b6a76225fc1eb859c0ffa08a408d07
Ivo Georgiev <ivo@linvo.org>
2015-08-04 23:58:46 +0300

    voutType, voutPtr

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 try { require('Common') } catch(e) { }
-var core = require("nodobjc/lib/core");
+
+var voutType, voutPtr;
 
 if (process.bridge && process.bridge.objc) { // so we can also test this in node.js
+  var core = require("nodobjc/lib/core");
   var $ = process.bridge.objc;
 
   var win = new Window();
@@ -16,21 +18,23 @@ if (process.bridge && process.bridge.objc) { // so we can also test this in node
   // TODO make nsView opaque
   */
 
-  // TODO: use CALayer to put the NSView on a black background
-
   var view = win.native('contentView');
 
   var layer = $.CALayer('alloc')('init');
   layer('setBackgroundColor', $.NSColor("blackColor")("CGColor"));
   view('setWantsLayer', $.YES);
   view('setLayer', layer); 
+
+  voutType = "nsobject";
+  voutPtr = core.unwrapValue(view, "@");
 }
 
 
 var WebChimera = require("./webchimera.js");
+// { vlcargs: [  ], voutType: voutType, voutPtr: voutPtr }
 var player = WebChimera.createPlayer([ /* "-vvv"  */ ]);
 //player.onFrameReady = function(frame) { typeof(display)=="function" && display() };
-if (view) player.setVout(core.unwrapValue(view, "@"));
+if (view) player.setVout(voutPtr);
 player.play("file:///Users/ivogeorgiev/Downloads/1.mkv");
 global.player = player;
 
