e11be46b8b11a40fbd14a8924cf1d2600ecc0d6c
Ivo Georgiev <ivo@linvo.org>
2015-08-07 21:03:26 +0300

    Implemented a second working approach to see if it improves perfomance - doesn't

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ try { require("Common") } catch(e) { }
 var voutType, voutPtr;
 
 if (process.bridge && process.bridge.objc) { // so we can also test this in node.js
+  // possible overheads  - not using VLC's desired interface to add/remove NSView's  - maybe that's why we can't handle 'resize' - maybe OS X begins to do some scaling 
+
   var core = require("nodobjc/lib/core");
   var $ = process.bridge.objc;
 
   var win = new Window();
   win.visible = true;
 
+  
   var backColor = $.NSColor("blackColor")
 
   var playerView = $.NSView.extend("playerView");
@@ -28,19 +31,43 @@ if (process.bridge && process.bridge.objc) { // so we can also test this in node
   win.native("contentView")("addSubview", view);
   win.native("contentView")("setAutoresizesSubviews", $.YES);
   view("setAutoresizingMask", $.NSViewHeightSizable | $.NSViewWidthSizable);
-  
-
-/*
-  var view = win.native("contentView");
-
-  var layer = $.CALayer("alloc")("init");
-  layer("setBackgroundColor", $.NSColor("blackColor")("CGColor"));
-  view("setWantsLayer", $.YES);
-  view("setLayer", layer); 
-*/
 
   voutType = "nsobject";
   voutPtr = core.unwrapValue(view, "@");
+  
+
+
+  /*
+  var layer = $.CALayer("alloc")("init");
+  layer("setBackgroundColor", $.NSColor("blackColor")("CGColor"));
+
+  var superView = win.native("contentView");
+  superView("setWantsLayer", $.YES);
+  superView("setLayer", layer); 
+
+  var proto = $.NSObject.extend("VLCEmbedder");
+  proto.addMethod("addVoutSubview:",{  retval: "@", args: ["@", ":", "@"] }, function(self, _cmd, view) {
+    var size = win.native("contentView")("frame").size;
+    view("setFrame", $.NSMakeRect(0,0,size.width,size.height));
+
+    superView("addSubview", view);
+    superView("setAutoresizesSubviews", $.YES);
+    superView("setAutoresizingMask", $.NSViewHeightSizable | $.NSViewWidthSizable);
+
+    console.log("add "+view)
+  });
+  
+  proto.addMethod("removeVoutSubview:",{  retval: "@", args: ["@", ":", "@"] }, function(self, _cmd, view) {
+    console.log("remove "+view)
+    //TODO
+  });
+  proto.register();
+
+  voutType = "nsobject";
+  voutPtr = core.unwrapValue(view = proto("alloc"), "@");
+  */
+
+
 }
 
 /*
@@ -56,7 +83,7 @@ win.visible = true;
 
 var WebChimera = require("./webchimera.js");
 // { vlcargs: [  ], voutType: voutType, voutPtr: voutPtr }
-var player = WebChimera.createPlayer([ /* "-vvv"  */ ]);
+var player = WebChimera.createPlayer([ /*"-vvv"*/  ]);
 //player.onFrameReady = function(frame) { typeof(display)=="function" && display() };
 if (view) player.setVout(voutPtr);
 player.play("file:///Users/ivogeorgiev/Downloads/1.mkv");
