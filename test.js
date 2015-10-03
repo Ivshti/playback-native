8633212c99339aad10e3a571637d15420f7cb458
Ivo Georgiev <ivo@linvo.org>
2015-10-03 11:38:55 +0300

    Experimentation with webview transparency

--- test.js
+++ test.js
@@ -2,8 +2,16 @@ try { require("Common") } catch(e) { }
 
 var WebView = require("WebView");
 var win = new Window();
+win.backgroundColor = 'rgba(0,0,0,0)';
+//win.frame = false;
+win.alpha = 1;
+
 var webview = new WebView();
 win.appendChild(webview);
 webview.left = webview.right = webview.top = webview.bottom = 0;
+//webview.backgroundColor = 'rgba(0,0,0,0)';
+webview.transparent = true;
+
 webview.location = "file://"+__dirname+"/test.html";
+//webview.location = "http://mobile.strem.io/";
 win.visible = true;
